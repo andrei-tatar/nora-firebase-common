6ced1e72add911e185efe24b7b50939c510bf9b8
Andrei Tatar <tatarandrei@gmail.com>
2021-01-19 21:25:51 +0200

    load schema function

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
 export * from './execute';
 export * from './device';
 export * from './checks';
+import { promises } from 'fs';
+import { join } from 'path';
+
+export async function loadSchema(name: 'device' | 'state') {
+    const buffer = await promises.readFile(join(__dirname, `${name}.json`));
+    return JSON.parse(buffer.toString());
+}
