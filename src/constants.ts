2434918de373ebc4c6238f4e06567a1e8af10e3d
Andrei Tatar <tatarandrei@gmail.com>
2021-08-13 14:36:41 +0300

    add constants for async commands

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,5 @@
+import { ErrorCode } from './device';
+
 export const HEARTBEAT_TIMEOUT_SEC = 60;
+export const ASYNC_CMD_TIMEOUT_ERRORCODE: ErrorCode = 'offline';
+export const ASYNC_CMD_TIMEOUT_MILLISECONDS = 1500;
