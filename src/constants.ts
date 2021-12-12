bf9a1b06495a59863abd65253398ddc16a36f958
Andrei Tatar <tatarandrei@gmail.com>
2021-12-12 11:38:01 +0200

    add sensor types that support notifications

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,12 @@ import { ErrorCode } from './device';
 export const HEARTBEAT_TIMEOUT_SEC = 60;
 export const ASYNC_CMD_TIMEOUT_ERRORCODE: ErrorCode = 'offline';
 export const ASYNC_CMD_TIMEOUT_MILLISECONDS = 1500;
+export const SENSOR_TYPE_NOTIFICATION_SUPPORT: ReadonlyArray<string> = [
+    'AirQuality',
+    'CarbonMonoxideLevel',
+    'SmokeLevel',
+    'FilterCleanliness',
+    'WaterLeak',
+    'RainDetection',
+    'FilterLifeTime',
+];
